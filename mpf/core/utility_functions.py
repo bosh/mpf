5af61bad856a3b39f5794c874564cd04c2aa2f1a
Alex Lobascio <alex@Lobasc.io>
2024-06-03 00:51:08 -0700

    add support of boolean settings
    values that cast to lowercase strings matching true/t/yes/enable/on or
    the number 1 will all be true, all other values will be treated as false

--- mpf/core/utility_functions.py
+++ mpf/core/utility_functions.py
@@ -48,6 +48,7 @@ class Util:
         # otherwise just cast to string
         return str(value)
 
+
     @staticmethod
     def convert_to_type(value, type_name):
         """Convert value to type."""
@@ -57,6 +58,8 @@ class Util:
             return float(value)
         if type_name == "str":
             return str(value)
+        if type_name == "bool":
+            return str(value.lower()) in ['true', 't', 'yes', 'enable', 'on', '1']
 
         raise AssertionError("Unknown type {}".format(type_name))
 
